af328e0e77680b772af7b7ea55b3763075aaa2d4
Abraham Jacob Pathil <abrahamjpathildev@gmail.com>
2025-10-25 09:50:27 +0530

    Update countingSort.c
    Line 10 - Have to include 1 else one element is out
    
    Lines 11 & 19 -> Uninitialised arrays have garbage values, fixed it using calloc
    
    Line 34 - Fixed incorrect indexing

--- countingSort.c
+++ countingSort.c
@@ -7,16 +7,16 @@ void countSort(int arr[], int size){
         if(arr[i] > max) max = arr[i];
         else if(arr[i] < min) min = arr[i];
     }
-    int freqArraySize = max - min;
-    int freqArray[freqArraySize];
+    int freqArraySize = max - min + 1;
+    int *freqArray = calloc(freqArraySize,sizeof(int));
     for(int i = 0; i < size; i++){
-        freqArray[arr[i]]++;
+        freqArray[arr[i]-min]++; //offset by min to avoid overflow
     } // for storing the frequency of occurence of each element
     for(int i = 1; i < freqArraySize ; i++){
         freqArray[i] = freqArray[i] + freqArray[i-1];
     } // for updating the array to hold positions
 
-    int sortedArray[size];
+    int *sortedArray = calloc(size,sizeof(int));
     int reqIndex;
     for(int i = size - 1; i >= 0; i--){
         reqIndex = --freqArray[arr[i]];
@@ -31,6 +31,6 @@ void countSort(int arr[], int size){
 void main(){
     int test[] = {23,4,33,1,5,9};
     countSort(test,6);
-    for(int i = 0; i < 5; i++) printf("%d ",test[i]);
+    for(int i = 0; i < 6; i++) printf("%d ",test[i]);
     printf("\n");
 }
