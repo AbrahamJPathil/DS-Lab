e20ad28bec84035e2e9c3acbbb61c5e77ddf4f0c
Abraham Jacob Pathil <abrahamjpathildev@gmail.com>
2025-11-01 18:35:46 +0530

    Update graphs.c
    Initial code for BFS; merged code from queue using linked list

--- General Data Structures/graphs.c
+++ General Data Structures/graphs.c
@@ -1,5 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
+// adding code for queue
+struct Node{
+	int data;
+	struct Node* next;
+};
+
+typedef struct Node Node;
+
+Node* createNode(int data){
+	Node* newNode = (Node*) malloc(sizeof(Node));
+	newNode -> next = NULL;
+	newNode -> data = data;
+	return newNode;
+}
+
+
+void insertQueue(Node** front, Node** rear,int data){
+	if(*front == *rear && *rear == NULL){
+		*front = createNode(data);
+		*rear = *front;
+		return;
+	}
+	else{
+		(*rear) -> next = createNode(data);
+		*rear = (*rear) -> next;
+		return;
+	}
+}
+
+void deleteQueue(Node** front, Node** rear){
+	if(*front == *rear && *rear == NULL){
+		printf("No element exists to delete \n");
+		return;
+	}
+	else if(*front == *rear){		// only one element found case
+		free(*front);
+		*front = NULL;
+		*rear = NULL;
+		return;
+	}
+	else{
+		Node* temp = *front;
+		*front = (*front) -> next;
+		free(temp);
+		return;
+	}
+}
+
+
+void displayAll(Node* front,Node* rear){
+	if(front == rear && rear == NULL){
+		printf("No element exists to display \n");
+		return;
+	}
+	else{
+		Node* temp = front;
+		while(temp -> next != NULL){
+			printf("%d -> ",temp -> data);
+			temp = temp -> next;
+		}
+		printf("%d",temp->data);
+	}
+}
+
+void deleteAll(Node** front,Node** rear){
+	if(*front == *rear && *rear == NULL){
+		printf("No element exists to delete \n");
+		return;
+	}
+	else{
+		Node* temp = *front;
+		Node* holder = temp;
+		while(temp -> next != NULL){
+			temp = temp -> next;
+			free(holder);
+			holder = temp;
+		}
+		free(holder);
+		*front = NULL;
+		*rear = NULL;
+		printf("Success!");
+	}
+}
+
+
+
+
 struct graph{
 	int n;
 	int** adjMatrix;
@@ -28,6 +115,33 @@ graph* createGraph(){
 	printf("Data entered successfully\n");
 	
 	return newGraph;
+
+
+void bfs(graph* gp,int startNode){
+	if(gp == NULL) return;
+	else if(gp -> adjMatrix == NULL) return;
+	Node* front = NULL;
+	Node* rear = NULL;
+	
+	int visited[gp->n] = {0}; //initialisation of an array
+	int currNode = startNode;
+	visited[currNode] = 1;
+	insertQueue(&front,&rear,currNode);
+	printf("%d ",startNode);
+	while(front != NULL && rear != NULL){
+		for(int i = 0; i < gp->n ; i++){
+			if(gp->adjMatrix[currNode][i] && visited[currNode] != 1){
+				insertQueue(&front,&rear,gp->adjMatrix[currNode][i]);
+				visited[currNode] = 1;
+				printf("%d ",gp->adjMatrix[currNode][i]);
+			
+			}
+		}
+		deleteQueue(&front,&rear);
+		currNode = front -> data;
+	}
+	printf("BFS Traversal Completed");
+}
 
 
 void main(){
