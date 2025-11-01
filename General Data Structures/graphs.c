66f4902a97040e76a3453657096fe1a1dee13ef6
Abraham Jacob Pathil <abrahamjpathildev@gmail.com>
2025-11-01 11:13:57 +0530

    array of pointers
    Created an array of pointers structure inside the main method, an additional for loop for allocating a 1D int array memory structure to each of these pointers, achieving a 2D int array structure, alongwith minor syntax fixes

--- General Data Structures/graphs.c
+++ General Data Structures/graphs.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 struct graph{
 	int n;
-	int* adjMatrix;
-}
+	int** adjMatrix;
+};
 
 typedef struct graph graph;
 
@@ -14,13 +14,16 @@ graph* createGraph(){
 	printf("Enter the number of nodes in graph: ");
 	scanf("%d",&n);
 	
-	graph* newGraph = malloc(sizeof(graph));
+	graph* newGraph = (graph*) malloc(sizeof(graph));
 	newGraph -> n = n;
-	newGraph -> adjMatrix = (int *) malloc(sizeof(int * n * n));
+	newGraph -> adjMatrix = (int **) malloc(sizeof(int*)*n); // allocate memory for an array of pointers
+	
+	for(int i = 0; i < n; i++)
+		newGraph -> adjMatrix[i] = (int *) malloc(sizeof(int)*n); // allocate memory for 1D element at each 							pointrr
 	
 	printf("Now enter the adjacency matrix for graph: ");
 	for(int i = 0; i < n ; i++){
-		for(int j = 0; j < n; j++)	scanf("%d",&(newGraph -> adjMatrix)[i][j]);
+		for(int j = 0; j < n; j++)	scanf("%d",&(newGraph -> adjMatrix[i][j]));
 	}
 	printf("Data entered successfully\n");
 	
